7708a9b7e174ba269aa37020c4a7ccf7f3e84786
Tuyrew12 <lzyforgame2@gmail.com>
2025-06-16 11:01:20 +0700

    Create v43g43ge3fv4vt34tb34b634tv43vt43tb43tb43tb43yt45tb45tv45tv3t43t45tb4tv34vt43tb34tb645t34tv43dsf32f32rv43tv43vt34tv34tv34vt34ctr34vt43tb45t4bt45bt54y765u7n56u56uy45vy54y547nuy754yt4f5t3c5t34tv34tb43b635t34rs342r34vr34b6t456n45yn45y45ny45bty45bty3v5t4t3v5t35t543tb543b6y7n5t43tb43.lua

---
+++ v43g43ge3fv4vt34tb34b634tv43vt43tb43tb43tb43yt45tb45tv45tv3t43t45tb4tv34vt43tb34tb645t34tv43t43tb43.lua
@@ -0,0 +1,718 @@
+
+-- [Second Webhook - 5 minute intervals]
+WebhookPNB2 = true
+WebhookLink2 = "https://discord.com/api/webhooks/1383886557525835806/fAjQwl4awVlIl99LpnmNuO7y65su2E53VztkezUNCjnJJ81KzAw_WGZJBvr1FPoATbSP"
+
+-- =========================
+--   END CONFIGURATION     --
+-- =========================
+-- [Role Account]
+RoleMVP = false
+RoleMOD = false
+
+--[DON'T SCROLL DOWN!]
+DelayConvertDL = DelayConvertDL or 0
+WorldName = GetWorld().name or "Unknown"
+Nick = GetLocal().name:gsub("`(%S)", ""):match("%S+")
+if AutoSuckGems then
+end
+NoGemsDrop = NoGemsDrop
+PinkGems = GetItemInfo("Pink Gemstone").id
+CheckIgnore = IgnoreCompletely and 1 or 0
+BlackGems = GetItemInfo("Black Gems").id
+DelayConvertBlack = DelayConvertDL * 50
+if RoleMOD then
+end
+RoleMVP = RoleMVP
+DelayConvertBGL = DelayConvertDL * 20
+CheckGems = NoGemsDrop and 1 or 0
+StartTime = os and os.time() or 0
+ConsumeItemID = {}
+Count = 1
+Now = 1
+GetRemote = false
+CheatOff = false
+CheatOn = false
+DBlock = false
+Ghost = false
+MagW = false
+Speed = 0
+IsConsuming = false
+LastWebhook2Time = 0
+RemoveHooks()
+
+function inv(id)
+  for _, item in pairs(GetInventory()) do
+    if item.id == id then
+      return item.amount
+    end
+  end
+  return 0
+end
+
+adjustedSitX, adjustedSitY = SitX, SitY
+
+-- Function to get state value based on direction
+function GetStateValue()
+  if StateDirection == "kiri" then
+    return 48
+  elseif StateDirection == "kanan" then
+    return 32
+  else
+    return 32 -- default to kanan if invalid value
+  end
+end
+
+AddHook("onvariant", "mommy", function(var)
+    if var[0] == "OnDialogRequest" and var[1]:find("add_player_info") then
+        if var[1]:find("|528|") then
+            consumeClover = true
+        else
+            consumeClover = false
+        end
+
+        if var[1]:find("|4604|") then
+            consumeArroz = true
+        else
+            consumeArroz = false
+        end
+
+        if var[1]:find("|1056|") then
+            consumeSongpyeon = true
+        else
+            consumeSongpyeon = false
+        end
+        return true
+    end
+    return false
+end)
+
+-- Function to adjust SitX and SitY based on Mode
+function AdjustSitCoordinates()
+  if Mode == "mneck" then
+    adjustedSitX = SitX + 1
+    adjustedSitY = SitY
+  elseif Mode == "vertical" then
+    adjustedSitX = SitX - 1
+    adjustedSitY = SitY
+  elseif Mode == "horizontal" then
+    adjustedSitX = SitX - 1
+    adjustedSitY = SitY - 1
+  else
+    adjustedSitX = SitX
+    adjustedSitY = SitY
+  end
+end
+
+function obj(id)
+  local total = 0
+  for _, object in pairs(GetObjectList()) do
+    if object.id == id then
+      total = total + object.amount
+    end
+  end
+  return total
+end
+
+PGems = pcall(obj) and obj(PinkGems) or 0
+BGems = pcall(obj) and obj(BlackGems) or 0
+Songpyeon = pcall(inv) and inv(1056) or 0
+Clover = pcall(inv) and inv(528) or 0
+Arroz = pcall(inv) and inv(4604) or 0
+BLK = pcall(inv) and inv(11550) or 0
+BGL = pcall(inv) and inv(7188) or 0
+DL = pcall(inv) and inv(1796) or 0
+TotalLocks = tonumber(BLK .. BGL .. DL)
+LockBefore = TotalLocks
+BGems = pcall(obj) and obj(BlackGems) or BGems
+BGemsBefore = BGems
+
+Songpyeon = inv(1056) or Songpyeon
+Clover = inv(528) or Clover
+Arroz = inv(4604) or Arroz
+if AutoConvertDL then
+  BLK = inv(11550) or 0
+  BGL = inv(7188) or 0
+  DL = inv(1796) or 0
+  TotalLocks = tonumber(BLK .. BGL .. DL)
+  LockBefore = TotalLocks
+end
+if not NoGemsDrop then
+  BGems = obj(BlackGems) or BGems
+  BGemsBefore = BGems
+end
+
+function GetMag(a, b)
+  tile = {}
+  for y = b, 0, -1 do
+    for x = a, 0, -1 do
+      if GetTile(x, y).fg == 5638 and GetTile(x, y).bg == MagBG then
+        table.insert(tile, {x = x, y = y})
+      end
+    end
+  end
+  return tile
+end
+
+function PathMag(x, y)
+  SendPacketRaw(false, {
+    state = GetStateValue(),
+    x = x * 32,
+    y = y * 32
+  })
+end
+
+-- Fungsi untuk PathSit (jalan ke kursi)
+function getSitXYForPath()
+  if Mode == "mneck" then
+    return SitX, SitY
+  elseif Mode == "vertical" then
+    return SitX - 1, SitY
+  elseif Mode == "horizontal" then
+    return SitX - 1, SitY - 1
+  else
+    return SitX, SitY
+  end
+end
+
+function getSitXYForConsume()
+  if Mode == "mneck" then
+    return SitX + 1, SitY
+  elseif Mode == "vertical" then
+    return SitX - 1, SitY
+  elseif Mode == "horizontal" then
+    return SitX - 1, SitY - 1
+  else
+    return SitX, SitY
+  end
+end
+
+function PathSit()
+  local x, y = getSitXYForPath()
+  SendPacketRaw(false, {
+    state = GetStateValue(),
+    x = x * 32 - 1,
+    y = y * 32 - 1
+  })
+end
+
+if GetTile(209, 0) then
+  Mag = GetMag(209, 209)
+elseif GetTile(199, 0) then
+  Mag = GetMag(199, 199)
+elseif GetTile(149, 0) then
+  Mag = GetMag(149, 149)
+elseif GetTile(99, 0) then
+  Mag = GetMag(99, 59)
+elseif GetTile(29, 0) then
+  Mag = GetMag(29, 29)
+end
+
+function Wrench(x, y)
+  SendPacketRaw(false, {
+    type = 3,
+    state = GetStateValue(),
+    value = 32,
+    px = x,
+    py = y,
+    x = x * 32,
+    y = y * 32
+  })
+end
+
+function SendWebhook(url, data)
+  MakeRequest(url, "POST", {
+    ["Content-Type"] = "application/json"
+  }, data)
+end
+
+function SendWebhook2()
+  if not WebhookPNB2 then return end
+  
+  local currentTime = os.time()
+  if currentTime - LastWebhook2Time < 300 then return end -- 5 minutes = 300 seconds
+  
+  LastWebhook2Time = currentTime
+  
+  -- Update current values
+  PGems = pcall(obj) and obj(PinkGems) or PGems
+  BGems = pcall(obj) and obj(BlackGems) or BGems
+  Songpyeon = pcall(inv) and inv(1056) or Songpyeon
+  Clover = pcall(inv) and inv(528) or Clover
+  Arroz = pcall(inv) and inv(4604) or Arroz
+  BLK = pcall(inv) and inv(11550) or BLK
+  BGL = pcall(inv) and inv(7188) or BGL
+  DL = pcall(inv) and inv(1796) or DL
+  
+  local Payload2 = [[
+{"embeds": [{
+"author": {"name": "PNB LOGS #REBANA",
+"icon_url": "https://cdn.discordapp.com/attachments/1349225845402894339/1380592004693622857/AAAAA.gif?ex=68466a40&is=684518c0&hm=394c453dd6c593ac3b744ec4bda1d0604e2d4d408e6e3243e8eab022c86fbe3d&"},
+"fields": [{"name": "<:AchievementSprites:1373112887203069972> Account",
+"value": "]] .. Nick .. [[
+",
+"inline": true},
+{"name": "<:growglobe:1382535597087785071> World",
+"value": "]] .. WorldName .. [[
+",
+"inline": true},
+{"name": "<:magplant:1368981774138605668> Magplant",
+"value": "]] .. Now .. " of " .. #Mag .. [[
+",
+"inline": false},
+{"name": "<:award:1373113752127537193> Consumables",
+"value": "]] .. Songpyeon .. " <:songpyeon:1368980154579157174> " .. Clover .. " <:clover:1368979672083464395> " .. Arroz .. [[
+ <:arroz:1368979942007902238>",
+"inline": true},
+{"name": "<:fwl:1373113385016758363> Total Locks",
+"value": "]] .. BLK .. " <a:irengb:1381540201955852359>  " .. BGL .. " <a:bglb:1381540210206314566>  " .. DL .. [[
+ <a:dlb:1381540213071024228>",
+"inline": true},
+{"name": "<:gemz:1382534859343401031> Gems Drop",
+"value": "]] .. FNum(BGems) .. "**<:blackgems:1376711562827534448>** " .. FNum(PGems) .. [[
+**<:pinkgems:1376711581383131157>**",
+"inline": false}],
+"footer": {"text": "Total PNB Time : ]] .. FTime(os.time() - StartTime) .. [[
+"},
+"color": ]] .. math.random(0, 16777215) .. [[
+}]
+}]]
+  
+  SendWebhook(WebhookLink2, Payload2)
+end
+
+function onvariant(var)
+  if "OnSDBroadcast" == var[0] then
+    if RoleMVP then
+      SendPacket(2, [[
+action|input
+|text|/radio]])
+    end
+    return true
+  elseif "OnTalkBubble" == var[0] and var[2]:find("You received a MAGPLANT 5000 Remote") then
+    GetRemote = true
+    CheatOn = true
+    DBlock = true
+    Count = 1
+  elseif "OnTalkBubble" == var[0] and var[2]:find("The MAGPLANT 5000 is empty") and not CheatOff then
+    GetRemote = false
+    CheatOff = true
+    MagW = false
+    Count = 1
+  elseif "OnConsoleMessage" == var[0] and var[1]:find("Where would you like to go") then
+    MagW = false
+    GetRemote = false
+    SendPacket(3, [[
+action|join_request
+name|]] .. WorldName .. [[
+|
+invitedWorld|0]])
+  elseif "OnConsoleMessage" == var[0] and var[1]:find("World Locked") then
+    if var[1]:find(WorldName) then
+      Count = 1
+      MagW = false
+      GetRemote = false
+    else
+      MagW = false
+      GetRemote = false
+      if not RoleMOD then
+        Ghost = true
+      end
+      SendPacket(3, [[
+action|join_request
+name|]] .. WorldName .. [[
+|
+invitedWorld|0]])
+    end
+  elseif "OnDialogRequest" == var[0] and GetRemote then
+    if var[1]:find("Wow, that's fast delivery.") then
+      return true
+    elseif var[1]:find("Welcome back") then
+      return true
+    elseif var[1]:find("add_player_info") and DBlock then
+      DBlock = false
+      return true
+    end
+  elseif "OnDialogRequest" == var[0] and not GetRemote then
+    if var[1]:find("ACTIVE") and var[1]:find(Mag[Now].x .. "\n") and var[1]:find(Mag[Now].y .. "\n") then
+      if var[1]:find("Seed") then
+        Now = Now == #Mag and 1 or Now + 1
+        PathMag(Mag[Now].x, Mag[Now].y - 1)
+      else
+        SendPacket(2, [[
+action|dialog_return
+dialog_name|magplant_edit
+x|]] .. Mag[Now].x .. [[
+|
+y|]] .. Mag[Now].y .. [[
+|
+buttonClicked|getRemote
+]])
+        if Mode == "horizontal" then
+          local x, y = SitX, SitY
+          FindPath(x - 1, y - 1)
+        else
+          PathSit()
+        end
+      end
+    elseif var[1]:find("DISABLED") and var[1]:find(Mag[Now].x .. "\n") and var[1]:find(Mag[Now].y .. "\n") then
+      if WebhookPNB and Now == #Mag then
+        SendWebhook(WebhookLink, "{\"content\": \"<@" .. DiscordID .. "> PNB Magplants is Empty!\"}")
+      end
+      Now = Now == #Mag and 1 or Now + 1
+      PathMag(Mag[Now].x, Mag[Now].y - 1)
+    end
+    return true
+  elseif "OnConsoleMessage" == var[0] and var[1]:find("Radio disabled,") and not RoleMVP then
+    SendPacket(2, [[
+action|input
+|text|/radio]])
+    return true
+  elseif "OnConsoleMessage" == var[0] and var[1]:find("Spam detected!") and not RoleMVP then
+    SendPacket(2, [[
+action|input
+|text|/radio]])
+    return true
+  elseif not (not ("OnConsoleMessage" == var[0] and var[1]:find("from")) or RoleMVP) or "OnNameChanged" == var[0] and RoleMVP then
+    if GetRemote then
+      if CheatOn then
+        if 0 == Count % (DelayRemote * 2) then
+          CheatOn = false
+          SendPacket(2, [[
+action|dialog_return
+dialog_name|cheats
+check_autofarm|1
+check_bfg|1
+check_gems|]] .. CheckGems .. [[
+
+check_lonely|]] .. CheckIgnore .. [[
+
+check_ignoreo|]] .. CheckIgnore .. [[
+
+check_ignoref|]] .. CheckIgnore)
+        end
+        Count = Count + 1
+      elseif AutoConvertDL then
+        if 0 == Count % DelayConvertBlack then
+          SendPacket(2, [[
+action|dialog_return
+dialog_name|info_box
+buttonClicked|make_bgl]])
+        end
+        Count = Count + 1
+      end
+      
+    elseif CheatOff then
+      if 0 == Count % (DelayRemote * 2) then
+        CheatOff = false
+        SendPacket(2, [[
+action|dialog_return
+dialog_name|cheats
+check_autofarm|0
+check_bfg|0
+check_gems|1
+check_lonely|]] .. CheckIgnore .. [[
+
+check_ignoreo|]] .. CheckIgnore .. [[
+
+check_ignoref|]] .. CheckIgnore)
+      end
+      Count = Count + 1
+    elseif MagW then
+      Wrench(Mag[Now].x, Mag[Now].y)
+    else
+      if 0 == Count % (DelayRemote * 3) and not Ghost then
+        PathMag(Mag[Now].x, Mag[Now].y - 1)
+        MagW = true
+      elseif 0 == Count % (DelayRemote * 2) and Ghost then
+        Ghost = false
+        SendPacket(2, [[
+action|input
+|text|/ghost]])
+      end
+      Count = Count + 1
+    end
+  end
+end
+
+function FNum(num)
+  num = tonumber(num)
+  if num >= 1.0E9 then
+    return string.format("%.2fB", num / 1.0E9)
+  elseif num >= 1000000.0 then
+    return string.format("%.1fM", num / 1000000.0)
+  elseif num >= 1000.0 then
+    return string.format("%.0fK", num / 1000.0)
+  else
+    return tostring(num)
+  end
+end
+
+function FTime(sec)
+  days = math.floor(sec / 86400)
+  hours = math.floor(sec % 86400 / 3600)
+  minutes = math.floor(sec % 3600 / 60)
+  seconds = math.floor(sec % 60)
+  if days > 0 then
+    return string.format("%sd %sh %sm %ss", days, hours, minutes, seconds)
+  elseif hours > 0 then
+    return string.format("%sh %sm %ss", hours, minutes, seconds)
+  elseif minutes > 0 then
+    return string.format("%sm %ss", minutes, seconds)
+  elseif seconds >= 0 then
+    return string.format("%ss", seconds)
+  end
+end
+
+function SendInfoPNB()
+  math.randomseed(os.time())
+  PGems = pcall(obj) and obj(PinkGems) or PGems
+  BGems = pcall(obj) and obj(BlackGems) or BGems
+  Songpyeon = pcall(inv) and inv(1056) or Songpyeon
+  Clover = pcall(inv) and inv(528) or Clover
+  Arroz = pcall(inv) and inv(4604) or Arroz
+  BLK = pcall(inv) and inv(11550) or BLK
+  BGL = pcall(inv) and inv(7188) or BGL
+  DL = pcall(inv) and inv(1796) or DL
+  if NoGemsDrop then
+    if AutoConvertDL then
+      TotalLocks = tonumber(BLK .. BGL .. DL)
+      Speed = string.format("%.2f DL", (TotalLocks - LockBefore) / 30)
+      LockBefore = TotalLocks
+    end
+  else
+    if BGems >= BGemsBefore * 1.5 then
+      Speed = string.format("%.2f BG", (BGems - BGemsBefore) / 30)
+    else
+      Speed = string.format("%.2f BG", BGems / 30)
+    end
+    BGemsBefore = BGems
+  end
+  Payload = [[
+{"embeds": [{
+"author": {"name": "PNB Bothax Andro & PC",
+"icon_url": "https://cdn.discordapp.com/attachments/1349225845402894339/1380592004693622857/AAAAA.gif?ex=68466a40&is=684518c0&hm=394c453dd6c593ac3b744ec4bda1d0604e2d4d408e6e3243e8eab022c86fbe3d&"},
+"fields": [{"name": "<:AchievementSprites:1373112887203069972> Account",
+"value": "]] .. Nick .. [[
+",
+"inline": false},
+{"name": "<:growglobe:1382535597087785071> World",
+"value": "]] .. WorldName .. [[
+",
+"inline": false},
+{"name": "<:magplant:1368981774138605668> Magplant",
+"value": "]] .. Now .. " of " .. #Mag .. [[
+",
+"inline": false},
+{"name": "<:award:1373113752127537193> Consumables",
+"value": "]] .. Songpyeon .. " <:songpyeon:1368980154579157174> " .. Clover .. " <:clover:1368979672083464395> " .. Arroz .. [[
+ <:arroz:1368979942007902238>",
+"inline": false},
+{"name": "<:fwl:1373113385016758363> Total Locks",
+"value": "]] .. BLK .. " <a:irengb:1381540201955852359>  " .. BGL .. " <a:bglb:1381540210206314566>  " .. DL .. [[
+ <a:dlb:1381540213071024228>",
+"inline": false},
+{"name": "<:gemz:1382534859343401031> Gems Drop",
+"value": "]] .. FNum(BGems) .. "**<:blackgems:1376711562827534448>** " .. FNum(PGems) .. [[
+**<:pinkgems:1376711581383131157>**",
+"inline": false}],
+"footer": {"text": "Total PNB Time : ]] .. FTime(os.time() - StartTime) .. [[
+"},
+"color": ]] .. math.random(0, 16777215) .. [[
+}]
+}]]
+  SendWebhook(WebhookLink, Payload)
+end
+
+function CheckBuffActive(buffId)
+  if buffId == 528 then
+    return consumeClover == true
+  elseif buffId == 4604 then
+    return consumeArroz == true
+  elseif buffId == 1056 then
+    return consumeSongpyeon == true
+  else
+    return false
+  end
+end
+
+local consumedFlags = {
+  [528] = false,
+  [4604] = false,
+  [1056] = false,
+}
+
+function Consumes()
+  if not GetRemote then 
+    IsConsuming = false
+    return 
+  end
+
+  if UseSongpyeon and Songpyeon > 0 and not CheckBuffActive(1056) and not consumedFlags[1056] then
+    IsConsuming = true
+    consumedFlags[1056] = true
+    local x, y = getSitXYForConsume()
+    SendPacketRaw(false, {
+      type = 3,
+      value = 1056,
+      px = x,
+      py = y,
+      x = x * 32,
+      y = y * 32
+    })
+    Sleep(200)
+    return
+  elseif UseClover and Clover > 0 and not CheckBuffActive(528) and not consumedFlags[528] then
+    IsConsuming = true
+    consumedFlags[528] = true
+    local x, y = getSitXYForConsume()
+    SendPacketRaw(false, {
+      type = 3,
+      value = 528,
+      px = x,
+      py = y,
+      x = x * 32,
+      y = y * 32
+    })
+    Sleep(200)
+    return
+  elseif UseArroz and Arroz > 0 and not CheckBuffActive(4604) and not consumedFlags[4604] then
+    IsConsuming = true
+    consumedFlags[4604] = true
+    local x, y = getSitXYForConsume()
+    SendPacketRaw(false, {
+      type = 3,
+      value = 4604,
+      px = x,
+      py = y,
+      x = x * 32,
+      y = y * 32
+    })
+    Sleep(200)
+    return
+  end
+
+  IsConsuming = false
+
+  if WebhookPNB then
+    SendInfoPNB()
+  end
+
+  if AutoSuckGems then
+    for i = 1, 3 do
+      SendPacket(2, [[
+action|dialog_return
+dialog_name|popup
+buttonClicked|bgem_suckall
+]])
+      Sleep(250)
+    end
+  end
+end
+
+function Overlay(text)
+  local var = {}
+  var[0] = "OnTextOverlay"
+  var[1] = "`w[`p@Rebana`w] `9" .. text
+  SendVariantList(var)
+end
+
+if os or not WebhookPNB then
+  if 0 == #Mag then
+    Overlay("`7Please Set Magplant Background")
+  else
+    Overlay("`2Script is working!")
+    Sleep(1000)
+    AddHook("onvariant", "onvariant", onvariant)
+    Sleep(1000)
+    if HideAnimation then
+      AddHook("onprocesstankupdatepacket", "OnIncomingRawPacket", function(pkt)
+        if 3 == pkt.type or 8 == pkt.type or 14 == pkt.type or 17 == pkt.type then
+          return true
+        end
+      end)
+      Sleep(1000)
+    end
+    if not RoleMVP then
+      SendPacket(2, [[
+action|input
+|text|/radio]])
+      Sleep(1000)
+    end
+    SendPacket(2, [[
+action|dialog_return
+dialog_name|cheats
+check_autofarm|0
+check_bfg|0
+check_gems|1
+check_lonely|]] .. CheckIgnore .. [[
+
+check_ignoreo|]] .. CheckIgnore .. [[
+
+check_ignoref|]] .. CheckIgnore)
+    Sleep(1000)
+    if RoleMVP or RoleMOD then
+      SendPacket(2, [[
+action|input
+|text|/modage 30]])
+      Sleep(1000)
+    end
+    function AutoConvertDLCheck()
+      if AutoConvertDL and not IsConsuming then
+        -- Update inventory values
+        BGL = inv(7188) or 0
+        DL = inv(1796) or 0
+        
+        -- Trigger 1: dlconvert - when gems reach threshold
+        local gemThreshold = EvadeTax and 100000 or 1000000
+        if GetPlayerInfo().gems > gemThreshold then
+          SendPacket(2, [[
+action|dialog_return
+dialog_name|telephone
+num|53785|
+x|]] .. (TelX - 1) .. [[
+|
+y|]] .. (TelY - 1) .. [[
+|
+buttonClicked|dlconvert]])
+          Sleep(700)
+        end
+        
+        -- Trigger 2: make_bgl - when there are 100+ BGL in inventory
+        if BGL >= 100 then
+          SendPacket(2, [[
+action|dialog_return
+dialog_name|info_box
+buttonClicked|make_bgl]])
+          Sleep(700)
+        end
+        
+        -- Trigger 3: bglconvert - when there are 100+ DL in inventory
+        if DL >= 100 then
+          SendPacket(2, [[
+action|dialog_return
+dialog_name|telephone
+num|53785|
+x|]] .. (TelX - 1) .. [[
+|
+y|]] .. (TelY - 1) .. [[
+|
+buttonClicked|bglconvert]])
+          Sleep(700)
+        end
+      end
+    end
+
+    while true do
+      Sleep(250) -- Add a small delay to prevent high CPU usage
+      Consumes()
+      Sleep(250) -- Add a small delay to prevent high CPU usage
+      AutoConvertDLCheck()
+      Sleep(250) -- Add a small delay to prevent high CPU usage
+      SendWebhook2() -- Send second webhook every 5 minutes
+      Sleep(250) -- Add a small delay to prevent high CPU usage
+    end
+  end
+else
+  Overlay("`7Turn On API MakeRequest & os for Webhook PNB")
+end
